60765e73052667e1f7eddf8d40a03fc8011812a6
peaprog <pigu.prasatko@snobicio.com>
2021-03-22 16:04:08 -0700

    we got the detection

--- uwu_converter.py
+++ uwu_converter.py
@@ -2,5 +2,9 @@
 
 normal_text = input("enter text to be uwufied: ")
 
-for o in normal_text:
-    print("uwu")
+char_list = ["a", "u" ,"o"]
+
+
+matched_list = [characters in char_list for characters in normal_text]
+print(matched_list)
+
