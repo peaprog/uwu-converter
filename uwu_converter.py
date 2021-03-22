60b7d83261646a655c068eb1af9419627e6cc7b6
unknown <justinxsds357@gmail.com>
2021-03-22 16:11:02 -0700

    lol

--- uwu_converter.py
+++ uwu_converter.py
@@ -8,3 +8,7 @@ char_list = ["a", "u" ,"o"]
 matched_list = [characters in char_list for characters in normal_text]
 print(matched_list)
 
+for "o" in normal_text:
+    UwU = normal_text.replace("o","Owo")
+    print(UwU)
+    
