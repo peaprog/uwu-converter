8e2911055432ae631e9e5f0326fc4c4e1b720e41
unknown <justinxsds357@gmail.com>
2021-04-05 15:30:15 -0700

    uwuwu

--- uwu_converter.py
+++ uwu_converter.py
@@ -1,21 +1,22 @@
 #epic owo converting thingy to make them memes
 
-normal_text = input("enter text to be uwufied: ")
+#normal_text = input("enter text to be uwufied: ")
 
-char_list = ["a", "u" ,"o"]
-
-
-matched_list = [characters in char_list for characters in normal_text]
-print(matched_list)
-
-Boolean in matched_list for o in normal_text:
-    if o == True:
-        UwU = normal_text.replace("o","Owo")
-        print(UwU)
+#char_list = ["a", "u" ,"o"]
 
 
-def uwu_converter(phrase):
+#matched_list = [characters in char_list for characters in normal_text]
+#print(matched_list)
+
+#Boolean in matched_list for o in normal_text:
+#    if o == True:
+#       UwU = normal_text.replace("o","Owo")
+#        print(UwU)
+
+
+def uwu_converter():
     translation = ""
+    phrase = input("Enter text to be uwufied: ")
     for letter in phrase:
         if letter.lower() in "aeiou":
             if letter.isupper():
