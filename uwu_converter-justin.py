b347753cb8ad7112eac23a32ec704439d30cf389
unknown <justinxsds357@gmail.com>
2021-04-05 16:00:54 -0700

    gottem

---
+++ uwu_converter-justin.py
@@ -0,0 +1,22 @@
+def uwu_converter():
+    translation = ""
+    phrase = input("Enter text to be uwufied or owofied: ")
+    for letter in phrase:
+        if letter.lower() in "u":
+            if letter.isupper():
+                translation = translation + "Uwu"
+            else:
+                translation = translation + "uwu"
+        elif letter.lower() in "aeio":
+            if letter.isupper():
+                translation = translation + "Owo"
+            else:
+                translation = translation + "owo"
+        else:
+            translation = translation + letter
+    return str(translation)
+
+while True:
+    try:
+        print("-----------------------------")
+        print(uwu_converter())
