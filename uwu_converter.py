f159e1d77d194c58ebafcf7ae5b4fbe7f0d629bb
unknown <justinxsds357@gmail.com>
2021-04-05 15:27:22 -0700

    uwu

--- uwu_converter.py
+++ uwu_converter.py
@@ -8,7 +8,20 @@ char_list = ["a", "u" ,"o"]
 matched_list = [characters in char_list for characters in normal_text]
 print(matched_list)
 
-for "o" in normal_text:
-    UwU = normal_text.replace("o","Owo")
-    print(UwU)
-    
+Boolean in matched_list for o in normal_text:
+    if o == True:
+        UwU = normal_text.replace("o","Owo")
+        print(UwU)
+
+
+def uwu_converter(phrase):
+    translation = ""
+    for letter in phrase:
+        if letter.lower() in "aeiou":
+            if letter.isupper():
+                translation = translation + "Owo"
+            else:
+                translation = translation + "owo"
+        else:
+            translation = translation + letter
+    return translate
