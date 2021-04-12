97ac4d0768c65ff59b353c1c98f467a0978ea230
peaprog <pigu.prasatko@snobicio.com>
2021-04-12 16:10:50 -0700

    added installer for noobs

---
+++ uwu-converter-installer.sh
@@ -0,0 +1,18 @@
+echo "uwu ------ Welcome to the uwu converter installer! ------- uwu"
+echo ""
+echo "Make sure you have github and python installed before running, and that this is the directory you want to install in. Otherwise press ctrl +c now!"
+
+sleep 10s
+
+echo "ok continuing"
+
+git clone https://github.com/peaprog/uwu-converter
+
+cd uwu-converter
+
+mv uwu-converter.sh ..
+
+echo ""
+echo "--------------------------------------"
+echo "Install succesful"
+echo "execute uwu-converter.sh to use!"
